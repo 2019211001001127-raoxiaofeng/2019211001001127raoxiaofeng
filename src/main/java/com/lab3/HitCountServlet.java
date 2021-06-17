015d1ae655fe2addfd11d2d378113d1ab40d11be
rao_xiao_feng <1978747724@qq.com>
2021-06-17 15:22:44 +0800

    Lab3 Commit

---
+++ src/main/java/com/lab3/HitCountServlet.java
@@ -0,0 +1,28 @@
+package com.lab3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HitCountServlet",value = "/HitCountServlet")
+public class HitCountServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<div style='text-align:center;'><h2>Total Number of Hits</h2></div>");
+        out.println("<div style='text-align:center;'><h4>27</h4></div>");
+    }
+}
