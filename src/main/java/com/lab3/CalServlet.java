aca0ccd751de4b66ddda8cbdf8c17a49648fb71c
rao_xiao_feng <1978747724@qq.com>
2021-06-17 15:23:37 +0800

    Lab3 Commit

--- src/main/java/com/lab3/CalServlet.java
+++ src/main/java/com/lab3/CalServlet.java
@@ -4,7 +4,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
-
+ 
 @WebServlet(name = "CalServlet", value = "/lab3/CalServlet")
 public class CalServlet extends HttpServlet {
     private int add(int firstVal,int secondVal) {
